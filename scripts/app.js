9ae1ce7cc860952127fe1f6e47d156e4b342329e
Roger Panella <rogerpanella@gmail.com>
2015-10-29 16:06:51 -0500

    changes to boilerplate

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// hey hey hey
+
+//
+window.onload = function () {
+  //alert('hey hey hey');
+  statusReport('hey HEY HEY!');
+}
+
+function statusReport(message) {
+   console.log(message);
+   return message;
+}
