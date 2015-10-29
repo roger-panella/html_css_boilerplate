f3e25f5da92f7f2ce6edd79c2046517a4ca2ed29
Roger Panella <rogerpanella@gmail.com>
2015-10-29 16:21:11 -0500

    changes to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,14 @@
-// hey hey hey
 
-//
 window.onload = function () {
   //alert('hey hey hey');
   statusReport('hey HEY HEY!');
+
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your mission?');
+
+var contentContainer = document.getElementById('content');
+
+contentContainer.innerHTML = username + ' is on a mission to ' + mission;
 }
 
 function statusReport(message) {
